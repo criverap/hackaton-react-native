67e0ebb3aec46c345bbb0913fe784918de06a4b4
criverap <crivera@thoughtworks.com>
2018-02-28 14:56:50 -0300

    [Cristina/Vicky] Add Error Modal to Login Screen

--- cuentaCuprum/src/views/Login/Login.js
+++ cuentaCuprum/src/views/Login/Login.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, Modal, Button } from 'react-native';
 import cssta from 'cssta/native'
 
 export default class Login extends React.Component {
 
 state = {
     rut:'',
-    password: ''
+    password: '', 
+    isError: false
+}
+
+openModal() {
+    this.setState({ isError: true })
+    this.setShadowBackground()
+}
+
+closeModal() {
+    this.setState({ isError: false })
+}
+
+setShadowBackground(){
+    return {
+        backgroundColor: '#DDD'
+    }
 }
 
     render() {
@@ -35,11 +51,27 @@ state = {
                     />
                 </View>
 
-                <Button
-                    color="#ffa409"
-                    onPress={() => this.props.navigation.navigate('Home')}
-                    title="Ingresar"
-                />
+                <TouchableOpacity
+                    onPress={() => this.openModal()} 
+                   // onPress={() => this.props.navigation.navigate('Home')} 
+                    style={styles.button}>
+                    <Text style={styles.buttonText}>Ingresar</Text>
+                </TouchableOpacity>
+            
+                <Modal
+                    visible={this.state.isError} transparent={true}
+                        animationType={'none'}
+                        onRequestClose={() => this.closeModal()}>
+                <View style={styles.errorModal}>
+                    <Text style={styles.modalText}>Datos Incorrectos</Text>
+                        <TouchableOpacity
+                            onPress={() => this.closeModal()}
+                            style={styles.button}>
+                            <Text style={styles.buttonText}>Aceptar</Text>
+                        </TouchableOpacity>
+                </View>
+                </Modal>
+          
             </View>
         );
     }
@@ -51,18 +83,11 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#FFF',
         flexDirection: 'column'
-       /* justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'stretch',
-        flexDirection: 'column',*/
     }, logo:{
         width:200,
         height:50,
         marginTop: 60,
         alignSelf: 'center',
-    }, completeButton:{
-        alignSelf: 'center',
-        marginTop: 40,
     }, title: {
         marginTop: 100,
         marginBottom: 5,
@@ -75,7 +100,31 @@ const styles = StyleSheet.create({
     }, inputs:{
         alignSelf:'center',
         marginBottom:50
+    }, button: {
+        width: 270,
+        height: 56,
+        alignSelf: 'center',
+        backgroundColor: '#ffa409',
+        justifyContent: 'center',
+        borderRadius: 5
+    }, buttonText:{
+        fontSize: 25,
+        color:'#fff',
+        alignSelf: 'center'
+    }, errorModal: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        height:140,
+        width:340,
+        marginTop:280,
+        alignSelf:'center',
+        borderWidth:1
+    }, modalText:{
+        fontSize: 18,
+        marginBottom: 15,
+        color:'#184371'
     }
-
 
 });

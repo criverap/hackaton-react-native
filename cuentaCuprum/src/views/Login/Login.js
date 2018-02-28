335190a3ffded1fba824490a28229f266770e94d
criverap <crivera@thoughtworks.com>
2018-02-28 11:09:38 -0300

    [Cristina/Vicky] Defining project structure

---
+++ cuentaCuprum/src/views/Login/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Login extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});
